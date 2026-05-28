abc857df536d273f3b6b952e682fd0a84cbb6ea4
Milo.Lee <milo.lee27@student.tamdistrict.org>
2026-05-28 13:48:13 -0700

    fix <APCSP.Number object at 0x0000016554589A90>

--- APCSP.py
+++ APCSP.py
@@ -304,6 +304,12 @@ class Number:
         self.value = value
         self.set_pos()
 
+    def __repr__(self):
+        return str(self.value)
+
+    # def __str__(self):
+    #     return str(self.value)
+
     def set_pos(self, pos_start=None, pos_end=None):
         self.pos_start = pos_start
         self.pos_end = pos_end
