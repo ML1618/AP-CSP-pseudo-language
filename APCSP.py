8390d1f4cbd9ad3484fe77af2c99f5854c9dc05e
Milo.Lee <milo.lee27@student.tamdistrict.org>
2026-06-01 10:56:09 -0700

    finish episode 3

--- APCSP.py
+++ APCSP.py
@@ -33,6 +33,29 @@ class InvalidSyntaxError(Error):
     def __init__(self, pos_start, pos_end, details):
         super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
 
+class RunTimeError(Error):
+    def __init__(self, pos_start, pos_end, details, context):
+        super().__init__(pos_start, pos_end, 'Runtime Error', details)
+        self.context = context
+
+    def as_string(self):
+        result = self.generate_traceback()
+        result += f'{self.error_name}: {self.details}'
+        result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
+        return result
+
+    def generate_traceback(self):
+        result = ''
+        pos = self.pos_start
+        ctx = self.context
+
+        while ctx:
+            result = f'File{pos.fn}, line{str(pos.ln + 1)}, in {ctx.display_name}\n' + result
+            pos = ctx.parent_entry_pos
+            ctx = ctx.parent
+
+        return 'Traceback (most recent call last):\n  ' + result
+
 ###############################
 # POSITION
 ##############################
@@ -296,6 +319,27 @@ class Parser:
         return res.success(left)
     
 ###############################
+# RUNTIME RESULT
+###############################
+
+class RunTimeResult:
+    def __init__(self):
+        self.value = None
+        self.error = None
+
+    def register(self, res):
+        if res.error: self.error = res.error
+        return res.value
+    
+    def success(self, value):
+        self.value = value
+        return self
+    
+    def failure(self, error):
+        self.error = error
+        return self
+
+###############################
 # VALUES
 ###############################
 
@@ -303,71 +347,101 @@ class Number:
     def __init__(self, value):
         self.value = value
         self.set_pos()
+        self.set_context()
 
     def __repr__(self):
         return str(self.value)
-
-    # def __str__(self):
-    #     return str(self.value)
 
     def set_pos(self, pos_start=None, pos_end=None):
         self.pos_start = pos_start
         self.pos_end = pos_end
         return self
     
+    def set_context(self, context=None):
+        self.context = context
+        return self
+    
     def added_to(self, other):
         if isinstance(other, Number):
-            return Number(self.value + other.value)
+            return Number(self.value + other.value).set_context(self.context), None
         
     def subtracted_by(self, other):
         if isinstance(other, Number):
-            return Number(self.value - other.value)
+            return Number(self.value - other.value).set_context(self.context), None
         
     def multiplied_by(self, other):
         if isinstance(other, Number):
-            return Number(self.value * other.value)
+            return Number(self.value * other.value).set_context(self.context), None
         
     def divided_by(self, other):
         if isinstance(other, Number):
-            return Number(self.value / other.value) # Possible divide by 0 error
+            if other.value == 0:
+                return None, RunTimeError(
+                    other.pos_start, other.pos_end,
+                    'Division by zero error',
+                    self.context
+                )
+            return Number(self.value / other.value).set_context(self.context), None
+        
+###############################
+# CONTEXT
+###############################
+
+class Context:
+    def __init__(self, display_name, parent=None, parent_entry_pos=None):
+        self.display_name = display_name
+        self.parent = parent
+        self.parent_entry_pos = parent_entry_pos
         
 ###############################
 # INTERPRETER
 ###############################
 
 class Interpreter:
-    def visit(self, node):
+    def visit(self, node, context):
         method_name = f'visit_{type(node).__name__}'
         method = getattr(self, method_name, self.no_visit_method)
-        return method(node)
-    
-    def no_visit_method(self, node):
+        return method(node, context)
+
+    def no_visit_method(self, node, context):
         raise Exception(f'No visit_{type(node).__name__} method defined')
     
-    def visit_NumberNode(self, node):
-        return Number(node.token.value).set_pos(node.pos_start, node.pos_end)
+    def visit_NumberNode(self, node, context):
+        return RunTimeResult().success(
+            Number(node.token.value).set_context(context).set_pos(node.pos_start, node.pos_end))
 
-    def visit_BinOpNode(self, node):
-        left = self.visit(node.left_node)
-        right = self.visit(node.right_node)
+    def visit_BinOpNode(self, node, context):
+        res = RunTimeResult()
+        left = res.register(self.visit(node.left_node, context))
+        if res.error: return res
+        right = res.register(self.visit(node.right_node, context))
 
         if node.op_token.type == TT_PLUS:
-            result = left.added_to(right)
+            result, error = left.added_to(right)
         elif node.op_token.type == TT_MINUS:
-            result = left.subtracted_by(right)
+            result, error = left.subtracted_by(right)
         elif node.op_token.type == TT_MULT:
-            result = left.multiplied_by(right)
+            result, error = left.multiplied_by(right)
         elif node.op_token.type == TT_DIV:
-            result = left.divided_by(right)
+            result, error = left.divided_by(right)
 
-        return result.set_pos(node.pos_start, node.pos_end)
+        if error:
+            return res.failure(error)
+        return res.success(result.set_pos(node.pos_start, node.pos_end))
 
-    def visit_UnaryOpNode(self, node):
-        number = self.visit(node.node)
+    def visit_UnaryOpNode(self, node, context):
+        res = RunTimeResult()
+        number = res.register(self.visit(node.node, context))
+        if res.error: return res
+
+        error = None
 
         if node.op_token.type == TT_MINUS:
             number = number.multiplied_by(Number(-1))
-        return number.set_pos(node.pos_start, node.pos_end)
+
+        if error:
+            return res.failure(error)
+        return res.success(number.set_pos(node.pos_start, node.pos_end))
 
 ###############################
 # RUN
@@ -386,6 +460,7 @@ def run(fn, text):
 
     # Run program
     interpreter = Interpreter()
-    result = interpreter.visit(ast.node)
+    context = Context('<program>')
+    result = interpreter.visit(ast.node, context)
 
-    return result, None
+    return result.value, result.error
